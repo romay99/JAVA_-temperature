6248b692d561b43b3b2bf24953eab1c75b53a848
SooBin_Lee <i_romay@naver.com>
2022-04-19 12:51:41 +0900

    method added

--- src/main.java
+++ src/main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sub subin = new sub();
 
+
+        //값을 변환할 값을 입력받아서 변수 value 에 저장.
+        System.out.println("값을 변환할 온도를 넣어주세요");
+        float value = sc.nextFloat();
+
+
+        //1번과 2번 선택지를 입력받음.
+        System.out.println("C > F 는 1번 , F > C 는 2번을 눌러주세요.");
+        int choice = sc.nextInt();
+
+        if(choice == 1)
+            System.out.println(subin.CtoF(value));
+        else if(choice == 2)
+            System.out.println(subin.FtoC(value));
+        else
+            System.out.println("잘못된 입력입니다. 프로그램이 종료됩니다.");
+
+
+            sc.close();
     }
 }
---
+++ src/sub.java
@@ -0,0 +1,16 @@
+public class sub {
+
+
+
+    //섭씨를 화씨로 변환하는 메서드
+    static float CtoF(float value){
+    return (value * 9/5) + 32f;
+    }
+
+
+    //화씨를 섭씨로 변환하는 메서드
+    static float FtoC(float value){
+       return (value -32f) * 5/9f;
+    }
+
+}
